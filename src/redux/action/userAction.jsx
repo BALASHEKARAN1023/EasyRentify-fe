142157b80b9267987316851487defa6d3d03b914
Bala <balashekaran2003@gmail.com>
2024-02-02 14:31:44 +0530

    update

--- src/redux/action/userAction.jsx
+++ src/redux/action/userAction.jsx
@@ -9,18 +9,18 @@ export const userLogin = (reqObj) => async dispatch => {
     try {
         const response = await axios.post('/api/users/login', reqObj)
         localStorage.setItem('users', JSON.stringify(response.data));
-        const roleChecker = response.data.role;
+        const roleChecker = await response.data.role;
         if (roleChecker === "admin") {
-            message.success('Login Success')
+           await message.success('Login Success')
             setTimeout(() => {
                 // navigate("/admin");
                 window.location.href = "/admin";
             }, 500);
         } else {
-            message.success('Login Success')
+           await message.success('Login Success')
             setTimeout(() => {
                 // navigate("/home");
-                window.location.href = "/home";
+               window.location.href = "/home";
             }, 500);
         }
 
@@ -35,11 +35,11 @@ export const userLogin = (reqObj) => async dispatch => {
 }
 
 export const userRegister = (reqObj) => async dispatch => {
-    const navigate=useNavigate();
+    const navigate=  useNavigate();
     dispatch({ type: 'LOADING', payload: true })
     try {
         await axios.post('/api/users/register', reqObj);
-        message.success('Registeration Success')
+        await message.success('Registeration Success')
         setTimeout(() => {
             navigate("/login");
             // window.location.href = "/login";
@@ -48,7 +48,7 @@ export const userRegister = (reqObj) => async dispatch => {
 
     } catch (error) {
         console.log(error);
-        message.error('Something went worng')
+        await message.error('Something went worng')
         dispatch({ type: "LOADING", payload: false })
 
     }
