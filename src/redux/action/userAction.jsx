93872ddeecbe51ab91502bc655d74805f5ade32a
Bala <balashekaran2003@gmail.com>
2024-02-02 12:16:23 +0530

    update

--- src/redux/action/userAction.jsx
+++ src/redux/action/userAction.jsx
@@ -1,10 +1,11 @@
 import { message } from "antd";
 import axios from "axios";
+import { useNavigate } from 'react-router-dom';
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "https://easyrentify.onrender.com";
 export const userLogin = (reqObj) => async dispatch => {
     dispatch({ type: 'LOADING', payload: true })
-
+    const navigate=useNavigate();
     try {
         const response = await axios.post('/api/users/login', reqObj)
         localStorage.setItem('users', JSON.stringify(response.data));
@@ -12,12 +13,14 @@ export const userLogin = (reqObj) => async dispatch => {
         if (roleChecker === "admin") {
             message.success('Login Success')
             setTimeout(() => {
-                window.location.href = "/admin";
+                navigate("/admin");
+                // window.location.href = "/admin";
             }, 500);
         } else {
             message.success('Login Success')
             setTimeout(() => {
-                window.location.href = "/home";
+                navigate("/home");
+                // window.location.href = "/home";
             }, 500);
         }
 
@@ -32,12 +35,14 @@ export const userLogin = (reqObj) => async dispatch => {
 }
 
 export const userRegister = (reqObj) => async dispatch => {
+    const navigate=useNavigate();
     dispatch({ type: 'LOADING', payload: true })
     try {
         await axios.post('/api/users/register', reqObj);
         message.success('Registeration Success')
         setTimeout(() => {
-            window.location.href = "/login";
+            navigate("/login");
+            // window.location.href = "/login";
         }, 500);
         dispatch({ type: 'LOADING', payload: false })
 
