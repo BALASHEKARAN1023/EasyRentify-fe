f8a50be47c2c13684d2a73fba6c2f65fc7ab8f3b
Bala <balashekaran2003@gmail.com>
2024-02-02 10:49:12 +0530

    thunk exported as default

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,6 +1,6 @@
 import { createStore,applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import {thunk} from 'redux-thunk';
+import thunk from 'redux-thunk';
 import { carsReducer } from './reducers/carsReducer';
 import  {alertsReducer}  from './reducers/AlertsReducer';
 import { bookingsReducer } from './reducers/bookingsReducer';
